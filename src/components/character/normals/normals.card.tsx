7caafb10bacc47cc017585981ecbc976ad909783
radamejr <50850876+radamejr@users.noreply.github.com>
2020-09-24 18:54:12 -0700

    further CSS updates, card added for normals

--- src/components/character/normals/normals.card.tsx
+++ src/components/character/normals/normals.card.tsx
@@ -1,5 +1,5 @@
-import { Card, CardContent } from '@material-ui/core';
 import React from 'react';
+import { Card, CardContent, Divider, Table, TableCell, TableContainer, TableRow, TableBody } from '@material-ui/core';
 import { Normal } from '../../../models/app';
 
 interface OwnProps {
@@ -9,15 +9,52 @@ type NormalsCardProps = OwnProps;
 
 const NormalsCard = (props: NormalsCardProps) => {
     const { normal } = props;
-    return (
-        <Card className={'normal-card'} variant="outlined">
-            <CardContent >
-                {normal?.input}
-            </CardContent>
-        </Card>
-            
-        
-    )
+    if(normal){
+        return (
+            <Card className="normal-card" variant="outlined">
+                <CardContent className="normal-content" >
+                    <div className="normal-image">
+                        <img src={normal.picture.url} alt={normal.input}/>
+                    </div>
+                    <Divider orientation="vertical" variant="middle"/>
+                    <div className="normal-data">
+                        <TableContainer className="normal-data-table-one" >
+                            <Table>
+                                <TableBody>
+                                    <TableRow>
+                                        <TableCell>Input:</TableCell>
+                                        <TableCell>{normal.input}</TableCell>
+                                        <TableCell>Recovery:</TableCell>
+                                        <TableCell>{normal.recovery}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>Startup:</TableCell>
+                                        <TableCell>{normal.startup}</TableCell>
+                                        <TableCell>Advantage:</TableCell>
+                                        <TableCell>{normal.advantage}</TableCell>
+                                    </TableRow>
+                                    <TableRow>
+                                        <TableCell>Active:</TableCell>
+                                        <TableCell>{normal.active}</TableCell>
+                                        <TableCell>Gaurd:</TableCell>
+                                        <TableCell>{normal.gaurd}</TableCell>
+                                    </TableRow>
+                                    <TableRow className="notes-row"> 
+                                        <TableCell colSpan={4}>
+                                            {`Notes: ${normal.special_notes}`}
+                                        </TableCell>
+                                    </TableRow>
+                                </TableBody>
+                            </Table>
+                        </TableContainer>
+                    </div>
+                </CardContent>
+            </Card>        
+        )
+    } else {
+        return null;
+    }
+    
 }   
 
 export default NormalsCard;
