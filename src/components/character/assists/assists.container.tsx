116f25017802f486522ad17eb2ca55d2a822215e
radamejr <50850876+radamejr@users.noreply.github.com>
2020-12-07 09:41:16 -0700

    sorted assists

--- src/components/character/assists/assists.container.tsx
+++ src/components/character/assists/assists.container.tsx
@@ -17,6 +17,19 @@ type AssistsContainerProps = OwnProps;
 const AssistsContainer = (props: AssistsContainerProps) => {
     const { assists, admin, character, mediaType } = props;
 
+    const sortAssists = (a: Assist, b: Assist) => {
+        const assistA = a.id
+        const assistB = b.id
+    
+        let comparison = 0;
+        if (assistA > assistB) {
+          comparison = 1;
+        }else if (assistA < assistB) {
+          comparison = -1;
+        }
+        return comparison;
+    } 
+
     const makeAssistCard = (assist: Assist, key: number) => {
         return ( 
             mediaType !== 'mobile' 
@@ -26,6 +39,9 @@ const AssistsContainer = (props: AssistsContainerProps) => {
             <AssistCardMobile assist={assist} key={key} admin={admin} character={character} />
         )
     }
+
+    const sortedAssists = assists?.sort(sortAssists);
+
     return (
         <div className={`assists-container ${mediaType}`}>
             <div className="assists-header">
@@ -42,9 +58,9 @@ const AssistsContainer = (props: AssistsContainerProps) => {
                 }
             </div>
             {
-                assists 
+                sortedAssists 
                 ?
-                assists.map((n: Assist, key: number) => { 
+                sortedAssists.map((n: Assist, key: number) => { 
                     return (
                     makeAssistCard(n, key)   
                     )                
